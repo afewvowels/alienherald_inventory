dc8f84db0f3bfbca3bb9deea70125b9bcd4c302f
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-07-22 04:43:56 -0400

    fixed dangerouslysetinnerhtml

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -28,7 +28,7 @@ class MyDocument extends Document {
             heap.load("${process.env.HEAP_ID}");)`}}
           />
           <script dangerouslySetInnerHTML={{__html:
-            `!function(f,b,e,v,n,t,s)
+            `(!function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -37,7 +37,7 @@ class MyDocument extends Document {
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '${process.env.PIXEL_ID}');
-            fbq('track', 'PageView');`}}
+            fbq('track', 'PageView');)`}}
             />
         </Head>
         <body>
