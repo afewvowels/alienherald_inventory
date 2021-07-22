d5cae8683fd05353b943e63adbaa2c081b48da2c
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2021-07-22 06:06:11 -0400

    add fb messenger

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -53,6 +53,31 @@ class MyDocument extends Document {
           />
           <Main />
           <NextScript />
+          {/*<!-- Messenger Chat Plugin Code -->*/}
+          <div id="fb-root"></div>
+
+          {/*<!-- Your Chat Plugin code -->*/}
+          <div id="fb-customer-chat" className="fb-customerchat"></div>
+
+          <script dangerouslySetInnerHTML={{__html:
+            `var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "105695311770374");
+            chatbox.setAttribute("attribution", "biz_inbox");
+
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v11.0'
+              });
+            };
+
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`}}/>
         </body>
       </Html>
     )
